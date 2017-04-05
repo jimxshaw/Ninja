a3e8f9f0646fd096d6f53b88396288de7ece0c05
Jim Shaw <jimxshaw@gmail.com>
2017-04-04 21:36:41 -0500

    Added sample project to test Code First Model from Db.

---
+++ Ninja.Demo1/CodeModelFromDb/Product.cs
@@ -0,0 +1,106 @@
+namespace CodeModelFromDb
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Production.Product")]
+    public partial class Product
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Product()
+        {
+            ProductCostHistories = new HashSet<ProductCostHistory>();
+            ProductInventories = new HashSet<ProductInventory>();
+            ProductListPriceHistories = new HashSet<ProductListPriceHistory>();
+            ProductProductPhotoes = new HashSet<ProductProductPhoto>();
+            ProductReviews = new HashSet<ProductReview>();
+        }
+
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string ProductNumber { get; set; }
+
+        public bool MakeFlag { get; set; }
+
+        public bool FinishedGoodsFlag { get; set; }
+
+        [StringLength(15)]
+        public string Color { get; set; }
+
+        public short SafetyStockLevel { get; set; }
+
+        public short ReorderPoint { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal StandardCost { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal ListPrice { get; set; }
+
+        [StringLength(5)]
+        public string Size { get; set; }
+
+        [StringLength(3)]
+        public string SizeUnitMeasureCode { get; set; }
+
+        [StringLength(3)]
+        public string WeightUnitMeasureCode { get; set; }
+
+        public decimal? Weight { get; set; }
+
+        public int DaysToManufacture { get; set; }
+
+        [StringLength(2)]
+        public string ProductLine { get; set; }
+
+        [StringLength(2)]
+        public string Class { get; set; }
+
+        [StringLength(2)]
+        public string Style { get; set; }
+
+        public int? ProductSubcategoryID { get; set; }
+
+        public int? ProductModelID { get; set; }
+
+        public DateTime SellStartDate { get; set; }
+
+        public DateTime? SellEndDate { get; set; }
+
+        public DateTime? DiscontinuedDate { get; set; }
+
+        public Guid rowguid { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ProductModel ProductModel { get; set; }
+
+        public virtual ProductSubcategory ProductSubcategory { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductCostHistory> ProductCostHistories { get; set; }
+
+        public virtual ProductDocument ProductDocument { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductInventory> ProductInventories { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductListPriceHistory> ProductListPriceHistories { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductProductPhoto> ProductProductPhotoes { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductReview> ProductReviews { get; set; }
+    }
+}
